facbf1b6acfe2074b41cae2d50179dd8c127c442
Rebert L. Walog Jr <112295364+rebertwalogjr@users.noreply.github.com>
2022-09-25 18:45:15 +0800

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    if (note.title === "") {
+      alert("Please input a title");
+    } else if (note.content == "") {
+      alert("Please provide a content");
+    } else {
+      props.onAdd(note);
+      setNote({
+        title: "",
+        content: ""
+      });
+    }
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
